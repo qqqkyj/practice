9ee69e66de609bcdfa463e1c7ac98c3ea0aa6f7d
qqqkyj <123rkdduswn@naver.com>
2025-09-30 14:07:09 +0900

    react list-rendering

--- react/my-react-app/src/App.jsx
+++ react/my-react-app/src/App.jsx
@@ -1,13 +1,10 @@
 import React from "react";
-import UserCardContainer from "./components/UserCard/UserCardContainer";
+import Carts from "./components/Carts/Carts";
 
 export default function App() {
 	return (
-		// <div>
-		// 	<h1 className="text-red-500">Hello World</h1>
-		// </div>
 		<>
-			<UserCardContainer></UserCardContainer>
+			<Carts></Carts>
 		</>
 	);
 }
